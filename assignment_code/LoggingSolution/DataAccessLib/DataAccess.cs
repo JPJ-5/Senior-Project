2709a34035a266646b7c5349217c8d763fb76253
Parth <officialparththanki@gmail.com>
2023-10-11 13:58:33 -0700

    Added and updated files in assignment_code folder

---
+++ assignment_code/LoggingSolution/DataAccessLib/DataAccess.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+
+namespace DataAccessLib
+{
+    public class DataAccess
+    {
+        private readonly string connectionString;
+
+        public DataAccess(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void SaveLog(string message)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("INSERT INTO Logs (Message) VALUES (@Message)", connection))
+                {
+                    command.Parameters.AddWithValue("@Message", message);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
