18317df0a3b8672124c21dc715e7575700f7f59e
StevenHall15000 <142917156+StevenHall15000@users.noreply.github.com>
2023-10-11 17:53:46 -0700

    Add files via upload

---
+++ assignment_code/Logging/StevenHall - Wanderer.CraftVerify/Wanderer.CraftVerify.Logging/Logger.cs
@@ -0,0 +1,19 @@
+﻿namespace Wanderer.CraftVerify.Logging
+{
+    using Wanderer.CraftVerify.DataAccess;
+    public class Logger
+    {
+        private DataAccess dataAccess;
+
+        public Logger(string connectionString)
+        {
+            dataAccess = new DataAccess(connectionString);
+        }
+
+        public void CreateLog(string logMessage)
+        {
+            string logData = $"[{DateTime.Now}] {logMessage}";
+            dataAccess.SaveDataToStore(logData);
+        }
+    }
+}
